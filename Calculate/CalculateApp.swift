d5b8066bb0a3c6dfafccce3f63f7de821007ed8d
lsunyoung <seongblis0504@gmail.com>
2023-01-15 18:43:57 +0900

    Initial Commit

---
+++ Calculate/CalculateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculateApp.swift
+//  Calculate
+//
+//  Created by 이선영 on 2023/01/15.
+//
+
+import SwiftUI
+
+@main
+struct CalculateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
